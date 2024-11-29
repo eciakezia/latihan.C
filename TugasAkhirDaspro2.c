38e54abf93dbf550a285f608e2ea83a6e2fed627
Kei <eciakezia@gmail.com>
2024-11-29 01:16:55 -0800

    Masukkan Pilihan

---
+++ TugasAkhirDaspro2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("======= Menu Belanja =======\n");
+    printf("1. Bahan Makanan\n");
+    printf("2. Peralatan Elektronik\n");
+    printf("3. Peralatan rumah tangga\n");
+    printf("4. Stationary\n");
+    printf("5. Exit\n");
+    printf("============================\n");
+
+    int pilihan;
+    printf("Masukkan pilihan (1-4): ");
+    scanf("%d", &pilihan);
+
+
+}
