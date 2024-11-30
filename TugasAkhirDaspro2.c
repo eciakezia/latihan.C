bcdca2a5fddb1341e239f0c98e9cf99c9ac16a74
Kei <eciakezia@gmail.com>
2024-11-30 12:00:08 -0800

    Menambah menu 2 & 4, juga mengubah menu 3

--- TugasAkhirDaspro2.c
+++ TugasAkhirDaspro2.c
@@ -50,45 +50,37 @@ void bahanMakanan(int *totalHarga, int *totalBarang){
     
 }
 
-//menu 3
-void peralatanRumahTangga(int *totalHarga, int *totalBarang){
-    int peralatan, jumlah, harga = 0;
+//menu 2
+void peralatanElektronik(int *totalHarga, int *totalBarang) {
+    int bahan, jumlah, harga = 0;
     char lanjut;
 
-    do{
+    do {
+        printf("===============================\n");
+        printf("1. Kipas Angin   Rp.150.000\n");
+        printf("2. Rice Cooker   Rp.250.000\n");
+        printf("3. Blender       Rp.300.000\n");
+        printf("4. Setrika       Rp.200.000\n");
+        printf("5. Lampu Lpj     Rp.50.000\n");
+        printf("6. mesin cuci    Rp.3.000.000\n");
+        printf("7. speaker       Rp.800.000\n");
+        printf("8. TV            Rp.3.000.000\n");
+        printf("===============================\n\n");
 
-        printf("==========================\n");
-        printf("1.  Meja      Rp.75.000\n");
-        printf("2.  Kursi     Rp.25.000\n");
-        printf("3.  Lemari    Rp.400.000\n");
-        printf("4.  Rak       Rp.50.000\n");
-        printf("5.  Kasur     Rp.500.000\n");
-        printf("6.  Tas       Rp.40.000\n");
-        printf("7.  Sendok    Rp.5.000\n");
-        printf("8.  Garpu     Rp.5.000\n");
-        printf("9.  Cermin    Rp.30.000\n");
-        printf("10. Sofa      Rp.300.000\n");
-        printf("==========================\n\n");
+        printf("Pilih (1-8) peralatan elektronik yang anda inginkan: ");
+        scanf("%d", &bahan);
 
-
-    printf("Pilih (1-10) Peralatan rumah tangga yang anda inginkan: ");
-    scanf("%d", &peralatan);
-
-
-        switch(peralatan){
-            case 1: harga = 75000; break;
-            case 2: harga = 25000; break;
-            case 3: harga = 400000; break;
-            case 4: harga = 50000; break;
-            case 5: harga = 500000; break;
-            case 6: harga = 40000; break;
-            case 7: harga = 5000; break;
-            case 8: harga = 5000; break;
-            case 9: harga = 30000; break;
-            case 10: harga = 300000; break;
+        switch (bahan) {
+            case 1: harga = 150000; break;
+            case 2: harga = 250000; break;
+            case 3: harga = 300000; break;
+            case 4: harga = 200000; break;
+            case 5: harga = 50000; break;
+            case 6: harga = 3000000; break;
+            case 7: harga = 800000; break;
+            case 8: harga = 3000000; break;
             default: printf("Pilihan tidak valid.\n");
-    }
-
+        }
         printf("Masukkan jumlah barang : ");
         scanf("%d", &jumlah);
 
@@ -97,12 +89,106 @@ void peralatanRumahTangga(int *totalHarga, int *totalBarang){
 
         printf("Anda membeli %d barang dengan total %d\n\n", jumlah, harga * jumlah);
 
-    printf("Apakah anda ingin memilih lagi peralatan rumah tangga? (y/t): ");
-    scanf(" %c", &lanjut);
+        printf("Apakah anda ingin memilih lagi peralatan elektronik? (y/t): ");
+        scanf(" %c", &lanjut);
 
-    }while (lanjut == 'y' || lanjut == 'Y');
+    }       while (lanjut == 'y' || lanjut == 'Y');
 
 }
+
+
+//menu 3
+void peralatanRumahTangga(int *totalHarga, int *totalBarang) {
+    int bahan, jumlah, harga = 0;
+    char lanjut;
+
+    do {
+        printf("==============================\n");
+        printf("1. Sapu ijuk.      Rp.25.000\n");
+        printf("2. Obat nyamuk.    Rp.10.000\n");
+        printf("3. Alat pel        Rp.32.000\n");
+        printf("4. Piring 1 lusin  Rp.24.000\n");
+        printf("5. Sendok 1 lusin  Rp.20.000\n");
+        printf("6. Gelas frozen.   Rp.28.000\n");
+        printf("7. Stela lemong    Rp.6.000\n");
+        printf("8. korek api       Rp.5.000\n");
+        printf("==============================\n\n");
+
+        printf("Pilih (1-8) peralatan rumah tangga yang anda inginkan: ");
+        scanf("%d", &bahan);
+
+        switch (bahan) {
+            case 1: harga = 25000; break;
+            case 2: harga = 10000; break;
+            case 3: harga = 32000; break;
+            case 4: harga = 24000; break;
+            case 5: harga = 20000; break;
+            case 6: harga = 28000; break;
+            case 7: harga = 6000; break;
+            case 8: harga = 5000; break;
+            default: printf("Pilihan tidak valid.\n");        
+        }
+    printf("Masukkan jumlah barang : ");
+        scanf("%d", &jumlah);
+
+        *totalHarga += harga * jumlah;
+        *totalBarang += jumlah;
+
+    printf("Anda membeli %d barang dengan total %d\n\n", jumlah, harga * jumlah);
+
+    printf("Apakah anda ingin memilih lagi peralatan rumah tangga? (y/t): ");
+        scanf(" %c", &lanjut);
+
+    }   while (lanjut == 'y' || lanjut == 'Y');
+
+}
+
+
+//menu 4
+ void stationary(int *totalHarga, int *totalBarang) {
+      int bahan, jumlah, harga = 0;
+      char lanjut;
+do {
+        printf("==============================\n");
+        printf("1. Buku 1 lusin    Rp.30.000\n");
+        printf("2. Pulpen 1 pack   Rp.21.000\n");
+        printf("3. pensil 1 pack   Rp.17.000\n");
+        printf("4. HVS A4 1 rim    Rp.55.000\n");
+        printf("5. Penggaris       Rp.14.000\n");
+        printf("6. Tip ex roll.    Rp.10.000\n");
+        printf("7. Pensil warna    Rp.38.000\n");
+        printf("8. spidol          Rp.9.000\n");
+        printf("==============================\n\n");
+
+        printf("Pilih (1-8) peralatan stationary yang anda inginkan: ");
+        scanf("%d", &bahan);
+
+    switch (bahan) {
+            case 1: harga = 30000; break;
+            case 2: harga = 21000; break;
+            case 3: harga = 17000; break;
+            case 4: harga = 55000; break;
+            case 5: harga = 14000; break;
+            case 6: harga = 10000; break;
+            case 7: harga = 38000; break;
+            case 8: harga = 9000 ; break;
+            default: printf("Pilihan tidak valid.\n");        
+        }
+        printf("Masukkan jumlah barang : ");
+        scanf("%d", &jumlah);
+
+        *totalHarga += harga * jumlah;
+        *totalBarang += jumlah;
+
+        printf("Anda membeli %d barang dengan total %d\n\n", jumlah, harga * jumlah);
+
+        printf("Apakah anda ingin memilih lagi peralatan stationary? (y/t): ");
+        scanf(" %c", &lanjut);
+
+    } while (lanjut == 'y' || lanjut == 'Y');
+
+}
+
 
 int main(){
     
@@ -113,7 +199,7 @@ int main(){
 
     do{
 
-    printf("======= Menu Belanja =======\n");
+    printf("\n\n======= Menu Belanja =======\n");
     printf("1. Bahan Makanan\n");
     printf("2. Peralatan Elektronik\n");
     printf("3. Peralatan rumah tangga\n");
@@ -128,11 +214,11 @@ int main(){
         switch(pilihan){
             case 1: bahanMakanan(&totalHarga, &totalBarang);
             break;
-            case 2:
+            case 2: peralatanElektronik(&totalHarga, &totalBarang);
             break;
             case 3: peralatanRumahTangga(&totalHarga, &totalBarang);
             break;
-            case 4:
+            case 4: stationary(&totalHarga, &totalBarang);
             break;
             case 5:
                 printf("Exited..");
